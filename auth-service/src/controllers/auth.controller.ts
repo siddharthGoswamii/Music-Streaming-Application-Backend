ffd86617cd73d047fd9c7cdc506e42516e89ccf9
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-06-15 14:17:12 +0530

    Refactor: Modified the status code for new user

--- auth-service/src/controllers/auth.controller.ts
+++ auth-service/src/controllers/auth.controller.ts
@@ -1,18 +1,32 @@
 import { Request, Response } from "express";
+import { registerUser } from "../services/auth.service";
 
 export const register = async (
-  req: Request,
-  res: Response
+  req: Request,
+  res: Response
 ) => {
-  try {
-    res.status(201).json({
-      success: true,
-      message: "Register API Working"
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Internal Server Error"
-    });
-  }
+
+  try {
+
+    const { name, email, password } = req.body;
+
+    const user = await registerUser(
+      name,
+      email,
+      password
+    );
+
+    res.status(201).json({
+      success: true,
+      user
+    });
+
+  } catch (error: any) {
+
+    res.status(400).json({
+      success: false,
+      message: error.message
+    });
+
+  }
 };
