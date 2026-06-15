9d6e424d1fb5dfa952c79fc3690a75c5b0b2e3d1
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-06-15 17:19:17 +0530

    Created login function

--- auth-service/src/services/auth.service.ts
+++ auth-service/src/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { pool } from "../config/db";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (
   name: string,
@@ -28,4 +29,50 @@ export const registerUser = async (
   );
 
   return result.rows[0];
+};
+
+export const loginUser = async (
+  email: string,
+  password: string
+) => {
+
+  const result = await pool.query(
+    "SELECT * FROM users WHERE email=$1",
+    [email]
+  );
+
+  if (result.rows.length === 0) {
+    throw new Error("Invalid Credentials");
+  }
+
+  const user = result.rows[0];
+
+  const isMatch = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+  if (!isMatch) {
+    throw new Error("Invalid Credentials");
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+      email: user.email
+    },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: "1d"
+    }
+  );
+
+  return {
+    token,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    }
+  };
 };
