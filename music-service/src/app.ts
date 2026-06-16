2c38a8251f2c1cfd0b9c88e719864d7576832603
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-06-16 16:20:47 +0530

    added the endpoint into the app.ts

--- music-service/src/app.ts
+++ music-service/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import artistRoutes from "./routes/artist.routes";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/artists", artistRoutes);
 
 app.get("/", (_, res) => {
   res.json({
