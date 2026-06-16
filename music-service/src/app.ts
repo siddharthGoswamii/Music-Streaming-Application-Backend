5d406f104006498b2eba2d6ddc80c854d4df8a75
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-06-16 14:11:48 +0530

    initiallized the music-service

---
+++ music-service/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.json({
+    success: true,
+    service: "Music Service"
+  });
+});
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+  console.log(`Music Service running on ${PORT}`);
+});
---
+++ music-service/src/config/db.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+pool.connect()
+  .then(() => console.log("Music DB Connected"))
+  .catch(console.error);
