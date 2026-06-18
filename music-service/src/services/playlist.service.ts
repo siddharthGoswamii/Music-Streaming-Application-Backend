ffa0d28906936f66e8b4c4b3e6b6730a389e998a
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-06-18 14:25:18 +0530

    created playlist service

---
+++ music-service/src/services/playlist.service.ts
@@ -0,0 +1,48 @@
+import { pool } from "../config/db";
+
+export const createPlaylist = async (
+  userId: string,
+  name: string,
+  description: string
+) => {
+    const result = await pool.query(
+        `
+        INSERT INTO playlists
+        (user_id,name,description)
+        VALUES($1,$2,$3)
+        RETURNING *
+        `,
+    [userId,name,description]
+    );
+    return result.rows[0];
+
+};
+
+export const getAllPlaylists = async () => {
+
+    const result = await pool.query(
+        `
+        SELECT *
+        FROM playlists
+        ORDER BY created_at DESC
+        `
+    );
+
+    return result.rows;
+};
+
+export const getPlaylistById = async (
+    id: string
+) => {
+
+    const result = await pool.query(
+        `
+        SELECT *
+        FROM playlists
+        WHERE id = $1
+        `,
+        [id]
+    );
+
+    return result.rows[0];
+};
