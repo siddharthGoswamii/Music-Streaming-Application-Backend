3c9f60e496ffe740e74c255c66a659c422bd01e6
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-06-22 13:42:37 +0530

    connected the routes

--- music-service/src/routes/track.routes.ts
+++ music-service/src/routes/track.routes.ts
@@ -4,7 +4,8 @@ import {
   addTrack,
   fetchTracks,
   fetchTrackById,
-  fetchTrendingTracks
+  fetchTrendingTracks,
+  searchTrack
 } from "../controllers/track.controller";
 
 const router = Router();
@@ -13,8 +14,11 @@ router.post("/", addTrack);
 
 router.get("/", fetchTracks);
 
+router.get("/search", searchTrack);
+
 router.get("/:id", fetchTrackById);
 
 router.get("/trending", fetchTrendingTracks);
+
 
 export default router;
