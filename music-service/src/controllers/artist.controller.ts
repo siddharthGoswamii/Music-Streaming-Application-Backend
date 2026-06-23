0f34d7f520a396b076e4bb24e4985d7a06848a3f
Siddharth Goswami <sidgoswami9891@gmail.com>
2026-06-23 16:55:27 +0530

    Put api for updating the artist in controller

--- music-service/src/controllers/artist.controller.ts
+++ music-service/src/controllers/artist.controller.ts
@@ -3,7 +3,8 @@ import type { Request, Response } from "express";
 import {
   createArtist,
   getAllArtists,
-  getArtistById
+  getArtistById,
+  updateArtist
 } from "../services/artist.service";
 
 export const addArtist = async (
@@ -106,4 +107,59 @@ export const fetchArtistById = async (
 
   }
 
+};
+
+//UPDATE ARTIST 
+
+export const editArtist = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+
+  try {
+
+    const { id } = req.params;
+
+    if (!id || typeof id !== 'string') {
+      res.status(400).json({
+        success: false,
+        message: 'Invalid track ID'
+      });
+      return;
+    }
+
+    const {
+      name,
+      bio,
+      image_url
+    } = req.body;
+
+    const artist = await updateArtist(
+      id,
+      name,
+      bio,
+      image_url
+    );
+
+    if (!artist) {
+      res.status(404).json({
+        success: false,
+        message: 'Artist not found'
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      artist
+    });
+
+  } catch (error: any) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
 };
